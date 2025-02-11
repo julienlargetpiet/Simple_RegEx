3ad7ddcb248b8815613e92009950e33c549d9e98
Julien <pgyygp090@gmail.com>
2025-02-11 17:56:57 +0100

    added regex_findr1-2sub functions

--- layer0.h
+++ layer0.h
@@ -371,3 +371,315 @@ std::map<std::vector<unsigned int>, std::map<bool, std::string>> regex_findr2sub
           {{cur_found, rtn_str}}}}; 
 };
 
+std::map<std::vector<unsigned int>, std::map<bool, std::string>> regex_findr1sub(std::string &searched, std::string &x) {
+  char cur_chr;
+  unsigned int i = 0;
+  unsigned int cnt = 0;
+  unsigned int ref_cnt;
+  unsigned int bf_cnt2;
+  const unsigned int n = searched.length();
+  const unsigned int n2 = x.length();
+  bool agn;
+  bool agn2;
+  unsigned int ref_int1;
+  unsigned int ref_int2;
+  bool range_state;
+  bool cur_found;
+  unsigned int rep_val;
+  unsigned int rep_val_cnt;
+  unsigned int ref_rep_val = 1;
+  unsigned int jump_i;
+  unsigned int jump_i2;
+  unsigned int jump_i3;
+  unsigned int idx_cnt = 0;
+  unsigned int cur_lmt;
+  unsigned int ref_cur_lmt;
+  unsigned int bf_rep_val;
+  bool greedy_state1 = 0;
+  bool greedy_state2 = 0;
+  std::string cur_matched_str = "";
+  unsigned int lst_cnt = -1;
+  unsigned int pre_cnt;
+  unsigned int cur_ref_cnt;
+  unsigned int temp_i;
+  bool is_repetition = 0;
+  unsigned int cur_idx;
+  unsigned int bf_cnt_zero = 0;
+  while (cnt < n2) {
+    greedy_state2 = 0;
+    if (searched[i] == '\\') {
+      i += 1;
+    };
+    rep_val = 1;
+    cur_found = 0;
+    range_state = 0;
+    ref_int1 = int(searched[i]);
+    if (i + 1 < n) {
+      if (searched[i + 1] == '-') {
+        i += 2;
+        if (!(i < n)) {
+          return {{{0, 0}, {{0, ""}}}};
+        };
+        range_state = 1;
+        ref_int2 = int(searched[i]);
+        while (!cur_found & cnt < n2) {
+          cur_found = (int(x[cnt]) >= ref_int1 & int(x[cnt]) <= ref_int2);
+          cnt += 1;
+        };
+      } else {
+        while (!cur_found & cnt < n2) {
+          cur_found = (ref_int1 == int(x[cnt])); 
+          cnt += 1;
+        };
+      };
+    } else {
+      while (!cur_found & cnt < n2) {
+        cur_found = (ref_int1 == int(x[cnt])); 
+        cnt += 1;
+      };
+    };
+    cur_matched_str = x[cnt - 1];
+    i += 1;
+    if (cur_found & i < n) {
+      if (searched[i] != '\\') {
+        if (searched[i] == '{') {
+          is_repetition = 1;
+          i += 1;
+          if (searched[i] == '+') {
+            greedy_state2 = 1;
+            i += 1;
+          };
+          if (searched[i] != '}') {
+            rep_val = int(searched[i]) - 48;
+            i += 1;
+            while (searched[i] != '}') {
+              rep_val *= 10;
+              rep_val += (int(searched[i]) - 48);
+              i += 1;
+            };
+          } else {
+            return {{{0, 0}, {{0, ""}}}};
+          };
+          i += 1;
+        };
+      } else {
+        i += 1;
+      };
+      if (greedy_state2 & rep_val == 0) {
+        rep_val = 1;
+      };
+      if (range_state & is_repetition & rep_val != 0 & ref_rep_val != 0) {
+        agn = 1;
+        bf_rep_val = rep_val;
+        rep_val *= ref_rep_val;
+        rep_val -= 1;
+        rep_val_cnt = 0;
+        while (rep_val_cnt < rep_val) {
+          agn2 = int(x[cnt]) >= ref_int1 & int(x[cnt]) <= ref_int2;
+          if (agn2) {
+            cur_matched_str.push_back(x[cnt]);
+            if (!(cnt + 1 < n2) & rep_val - rep_val_cnt > 1) {
+              agn = 0;
+              break;
+            } else {
+              cnt += 1;
+            };
+            rep_val_cnt += 1;
+          } else {
+            agn = 0;
+            break;
+          };
+        };
+        if (!agn) {
+          cur_found = 0;
+        } else if (greedy_state1 || greedy_state2) {
+          if (greedy_state1 & greedy_state2) {
+            cur_lmt = bf_rep_val + ref_rep_val;
+          } else if (greedy_state1) {
+            cur_lmt = bf_rep_val;
+          } else if (greedy_state2) {
+            cur_lmt = ref_rep_val;
+          };
+          while (agn) {
+            rep_val_cnt = 0;
+            cur_ref_cnt = cnt;
+            while (rep_val_cnt < cur_lmt) {
+              agn2 = int(x[cnt]) >= ref_int1 & int(x[cnt]) <= ref_int2;
+              if (agn2) {
+                cur_matched_str.push_back(x[cnt]);
+                if (!(cnt + 1 < n2) & cur_lmt - rep_val_cnt > 1) {
+                  agn = 0;
+                  break;
+                } else {
+                  cnt += 1;
+                };
+                rep_val_cnt += 1;
+              } else {
+                agn = 0;
+                break;
+              };
+            };
+          };
+          cnt = cur_ref_cnt;
+        };
+        rep_val = 1;
+      } else if (is_repetition & rep_val != 0 & ref_rep_val != 0) {
+        agn = 1;
+        bf_rep_val = rep_val;
+        rep_val *= ref_rep_val;
+        rep_val -= 1;
+        rep_val_cnt = 0;
+        while (rep_val_cnt < rep_val) {
+          agn2 = int(x[cnt]) == ref_int1;
+          if (agn2) {
+            cur_matched_str.push_back(x[cnt]);
+            if (!(cnt + 1 < n2) & rep_val - rep_val_cnt > 1) {
+              agn = 0;
+              break;
+            } else {
+              cnt += 1;
+            };
+            rep_val_cnt += 1;
+          } else {
+            agn = 0;
+            break;
+          };
+        };
+        if (!agn) {
+          cur_found = 0;
+        } else if (greedy_state1 || greedy_state2) {
+          if (greedy_state1 & greedy_state2) {
+            cur_lmt = bf_rep_val + ref_rep_val;
+          } else if (greedy_state1) {
+            cur_lmt = bf_rep_val;
+          } else if (greedy_state2) {
+            cur_lmt = ref_rep_val;
+          };
+          while (agn) {
+            rep_val_cnt = 0;
+            cur_ref_cnt = cnt;
+            while (rep_val_cnt < cur_lmt) {
+              agn2 = int(x[cnt]) == ref_int1;
+              if (agn2) {
+                cur_matched_str.push_back(x[cnt]);
+                if (!(cnt + 1 < n2) & cur_lmt - rep_val_cnt > 1) {
+                  agn = 0;
+                  break;
+                } else {
+                  cnt += 1;
+                };
+                rep_val_cnt += 1;
+              } else {
+                agn = 0;
+                break;
+              };
+            };
+          };
+          cnt = cur_ref_cnt;
+        };
+        rep_val = 1;
+      };
+      bf_cnt2 = cnt;
+    } else {
+      if (ref_rep_val == 0) {
+        if (cur_found) {
+          cur_found = 0;
+        } else {
+          cur_found = 1;
+          bf_cnt2 = cnt;
+        };
+        if (i + 3 < n) {
+          if (searched[i] == '{' & searched[i + 1] == '0' & searched[i + 2] == '}') {
+            i += 3;
+          } else if (searched[i] == '{' & searched[i + 1] == '+' & searched[i + 2] == '0' & searched[i + 3] == '}') {
+            i += 4;
+          };
+        } else if (i + 3 == n) {
+          if (searched[i] == '{' & searched[i + 1] == '0' & searched[i + 2] == '}') {
+            i += 3;
+          };
+        };
+      } else if (i + 3 < n) {
+        if (searched[i] == '{' & searched[i + 1] == '0' & searched[i + 2] == '}') {
+          if (cur_found) {
+            cur_found = 0;
+          } else {
+            cur_found = 1;
+            bf_cnt2 = cnt;
+          };
+          i += 3;
+        } else if (searched[i] == '{' & searched[i + 1] == '+' & searched[i + 2] == '0' & searched[i + 3] == '}') {
+          if (cur_found) {
+            cur_found = 0;
+          } else {
+            cur_found = 1;
+            bf_cnt2 = cnt;
+          };
+          i += 4;
+        };
+      } else if (i + 3 == n) {
+        if (searched[i] == '{' & searched[i + 1] == '0' & searched[i + 2] == '}') {
+          if (cur_found) {
+            cur_found = 0;
+          } else {
+            cur_found = 1;
+            bf_cnt2 = cnt;
+          };
+          i += 3;
+        };
+      };
+    };
+    if (cnt < n2 & cur_found) {
+      if (range_state) {
+        agn2 = int(x[cnt]) >= ref_int1 & int(x[cnt]) <= ref_int2;
+        if (agn2) {
+          for (temp_i = 0; temp_i + 1 < cur_matched_str.length(); ++temp_i) {
+            cur_matched_str[temp_i] = cur_matched_str[temp_i + 1];   
+          };
+          cur_matched_str[cur_matched_str.length() - 1] = x[cnt];
+          cnt += 1;
+          while (agn2 & cnt < n2) {
+            agn2 = int(x[cnt]) >= ref_int1 & int(x[cnt]) <= ref_int2;
+            if (agn2) {
+              for (temp_i = 0; temp_i + 1 < cur_matched_str.length(); ++temp_i) {
+                cur_matched_str[temp_i] = cur_matched_str[temp_i + 1];   
+              };
+              cur_matched_str[cur_matched_str.length() - 1] = x[cnt];
+              cnt += 1;
+            };
+          };
+        };
+      } else {
+        agn2 = int(x[cnt]) == ref_int1;
+        if (agn2) {
+          cnt += 1;
+          while (agn2 & cnt < n2) {
+            agn2 = int(x[cnt]) == ref_int1;
+            if (agn2) {
+              cnt += 1;
+            };
+          };
+        };
+      };
+    };
+    if (rep_val == 0 || ref_rep_val == 0) {
+      if (bf_cnt_zero + 1 < bf_cnt2) {
+        lst_cnt = bf_cnt_zero;
+        pre_cnt = lst_cnt;
+        break;
+      };
+      bf_cnt_zero = cnt;
+    };
+    if (cur_found & rep_val != 0 & ref_rep_val != 0) {
+      lst_cnt = cnt - 1;
+      pre_cnt = cnt - cur_matched_str.length();
+      break;
+    };
+    i = 0;
+  };
+  if (lst_cnt == -1) {
+    return {{{0, 0}, {{0, ""}}}};
+  };
+  return {{{pre_cnt, lst_cnt}, 
+          {{cur_found, cur_matched_str}}}}; 
+};
