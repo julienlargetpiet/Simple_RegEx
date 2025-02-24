5f53733601307d7bbf56da7072773a47e5b9bbb5
Julien <pgyygp090@gmail.com>
2025-02-24 13:53:52 +0100

    updated regex_findrmid function

--- layer4.h
+++ layer4.h
@@ -249,32 +249,33 @@ std::map<std::vector<int>, std::map<bool, std::string>> regex_findrmid(std::stri
     } else if (or_context) {
       if (conditions_idxv2.size() > 0) {
         if (cnt - 1 == conditions_idxv2[idx_condition]) {
-          
-          cur_cur_searched = cur_searched[cur_searched.length() - 1];
-          cur_searched.pop_back();
-          if (cur_searched.length() > 1) {
-            if (cur_searched[cur_searched.length() - 1] == '-') {
-              cur_cur_searched.insert(0, 1, '-');
-              cur_searched.pop_back();
-              cur_cur_searched.insert(0, 1, cur_searched[cur_searched.length() - 1]);
-              cur_searched.pop_back();
-            };
+          cur_searched.insert(0, 1, '[');
+          cur_searched.push_back(']');
+          cur_searched.push_back('{');
+          cur_searched += std::to_string(ref_mult);
+          cur_searched.push_back('}');
+          cur_x = "";
+          for (cnt2 = rtn_lst_cnt + 1; cnt2 < n2; ++cnt2) {
+            cur_x.push_back(x[cnt2]);
           };
-          ok_next = 1;
-          if (cur_searched.length() > 0) {
-            cur_mp = regex_findr2sub(cur_searched, cur_x);
-            cur_it = cur_mp.begin();
-            rslt_mp = cur_it->second.begin();
-            if (rslt_mp->first) {
-              cur_str = rslt_mp->second;
-              rtn_str += cur_str;
-              rtn_lst_cnt += cur_str.length();
-              cur_x = "";
-              for (cnt2 = rtn_lst_cnt + 1; cnt2 < n2; ++cnt2) {
-                cur_x.push_back(x[cnt2]);
-              };
-            } else {
-              ok_next = 0;
+          cur_mp = regex_findr2sub(cur_searched, cur_x);
+          cur_it = cur_mp.begin();
+          rslt_mp = cur_it->second.begin();
+          if (rslt_mp->first) {
+            cur_str = rslt_mp->second;
+            rtn_str += cur_str;
+            rtn_lst_cnt += cur_str.length();
+            cur_x = "";
+            n2 = n2_ref;
+            for (cnt2 = rtn_lst_cnt + 1; cnt2 < n2; ++cnt2) {
+              cur_x.push_back(x[cnt2]);
+            };
+            cnt = jump_cnt;
+            or_context = 0;
+            is_mult = 0;
+          } else {
+            idx_condition += 1;
+            if (cnt == conditions_idx) {
               cur_hmn_idx += 1;
               cnt = ref_cnt;
               if (cur_hmn_idx == hmn_idxv[cur_hmn].size()) {
@@ -290,139 +291,128 @@ std::map<std::vector<int>, std::map<bool, std::string>> regex_findrmid(std::stri
                 rtn_lst_cnt = hmn_idxv[cur_hmn][cur_hmn_idx];
               };
             };
-            cur_searched = "";
           };
-          if (ok_next) {
-            idx_condition += 1;
-            cur_cur_searched.insert(0, 1, '[');
-            cur_cur_searched.push_back(']');
-            cur_cur_searched.push_back('{');
-            cur_cur_searched += std::to_string(ref_mult);
-            cur_cur_searched.push_back('}');
-            cur_x = "";
-            for (cnt2 = rtn_lst_cnt + 1; cnt2 < n2; ++cnt2) {
-              cur_x.push_back(x[cnt2]);
-            };
-            cur_mp = regex_findr2sub(cur_cur_searched, cur_x);
-            cur_it = cur_mp.begin();
-            rslt_mp = cur_it->second.begin();
-            if (rslt_mp->first) {
-              cur_str = rslt_mp->second;
-              rtn_str += cur_str;
-              rtn_lst_cnt += cur_str.length();
-              cur_x = "";
-              n2 = n2_ref;
-              for (cnt2 = rtn_lst_cnt + 1; cnt2 < n2; ++cnt2) {
-                cur_x.push_back(x[cnt2]);
-              };
-              cnt = jump_cnt;
-              or_context = 0;
-              is_mult = 0;
-            } else {
-              if (cnt == conditions_idx) {
-                cur_hmn_idx += 1;
-                cnt = ref_cnt;
-                if (cur_hmn_idx == hmn_idxv[cur_hmn].size()) {
-                  cur_hmn_idx = 0;
-                  cur_hmn += 1;
-                  if (cur_hmn < n_hmn) {
-                    rtn_str = hmn_str_idxv[cur_hmn][cur_hmn_idx];
-                    rtn_lst_cnt = hmn_idxv[cur_hmn][cur_hmn_idx];
-                  } else {
-                    return {{{0, 0}, {{0, ""}}}};
-                  };
-                } else {
-                  rtn_lst_cnt = hmn_idxv[cur_hmn][cur_hmn_idx];
-                };
-              };
-            };
-          };
+          cur_searched = "";
         };
       };
     } else if (searched[cnt] == '[' & searched[cnt - 1] != '\\') {
-      idx_condition = 0;
-      is_mult = 0;
-      conditions_idxv2 = {};
-      temp_cnt = cnt + 1;
-      or_context = 1;
-      ref_mult = 1;
-      while (1) {
-        if (searched[temp_cnt] == ']' & searched[temp_cnt - 1] != '\\') {
-          break;
-        } else if (searched[temp_cnt + 1] == '{' & searched[temp_cnt] != '\\') {
-           temp_cnt += 1;
-           if (searched[temp_cnt + 1] != '?') {
-             temp_cnt += 1;
-             while (searched[temp_cnt] != '}') {
-               temp_cnt += 1;
-             };
-             conditions_idxv2.push_back(temp_cnt);
-           } else {
-             temp_cnt += 1;
-             cur_par = 1;
-             while (1) {
-               if (searched[temp_cnt] == '}' & searched[temp_cnt - 1] != '\\') {
-                 cur_par -= 1;
-                 if (cur_par == 0) {
-                   break;
-                 };
-               } else if (searched[temp_cnt] == '{' & searched[temp_cnt - 1] != '\\') {
-                 cur_par += 1;
-               };
-               temp_cnt += 1;
-             };
-           };
+      ok_next = 1;
+      if (cur_searched.length() > 0) {
+        cur_mp = regex_findr2sub(cur_searched, cur_x);
+        cur_it = cur_mp.begin();
+        rslt_mp = cur_it->second.begin();
+        if (rslt_mp->first) {
+          cur_str = rslt_mp->second;
+          rtn_str += cur_str;
+          rtn_lst_cnt += cur_str.length();
+          cur_x = "";
+          for (cnt2 = rtn_lst_cnt + 1; cnt2 < n2; ++cnt2) {
+            cur_x.push_back(x[cnt2]);
+          };
         } else {
-          if (searched[temp_cnt + 1] == '-') {
-            temp_cnt += 2;
-          };
-          conditions_idxv2.push_back(temp_cnt);
-        };
-        temp_cnt += 1;
-      };
-      conditions_idx = temp_cnt;
-      temp_cnt += 1;
-      if (temp_cnt < n) {
-        if (searched[temp_cnt] == '{') {
-          temp_cnt += 1;
-          if (searched[temp_cnt] == '?') {
-            cur_break_ptrn = "";
-            temp_cnt += 1;
-            cur_par = 1;
-            while (1) {
-              if (searched[temp_cnt] == '}' & searched[temp_cnt - 1] != '\\') {
-                cur_par -= 1;
-                if (cur_par == 0) {
-                  break;
-                };
-              } else if (searched[temp_cnt] == '{' & searched[temp_cnt - 1] != '\\') {
-                cur_par += 1;
-              };
-              cur_break_ptrn.push_back(searched[temp_cnt]);
-              temp_cnt += 1;
+          ok_next = 0;
+          cur_hmn_idx += 1;
+          cnt = ref_cnt;
+          if (cur_hmn_idx == hmn_idxv[cur_hmn].size()) {
+            cur_hmn_idx = 0;
+            cur_hmn += 1;
+            if (cur_hmn < n_hmn) {
+              rtn_str = hmn_str_idxv[cur_hmn][cur_hmn_idx];
+              rtn_lst_cnt = hmn_idxv[cur_hmn][cur_hmn_idx];
+            } else {
+              return {{{0, 0}, {{0, ""}}}};
             };
-            cur_mp = regex_findrlow(cur_break_ptrn, cur_x);
-            cur_it = cur_mp.begin();
-            rslt_mp = cur_it->second.begin();
-            if (rslt_mp->first) {
-              idx_v = cur_it->first;
-              n2 = idx_v[0];
-            };
-            temp_cnt += 1;
           } else {
-            is_mult = 1;
-            ref_mult = int(searched[temp_cnt]) - 48;
-            while (searched[temp_cnt] != '}') {
-              ref_mult *= 10;
-              ref_mult += (int(searched[temp_cnt]) - 48);
-              temp_cnt += 1;
-            };
-            temp_cnt += 1;
+            rtn_lst_cnt = hmn_idxv[cur_hmn][cur_hmn_idx];
           };
         };
+        cur_searched = "";
       };
-      cnt += 1;
-      jump_cnt = temp_cnt;
+      if (ok_next) {
+        idx_condition = 0;
+        is_mult = 0;
+        conditions_idxv2 = {};
+        temp_cnt = cnt + 1;
+        or_context = 1;
+        ref_mult = 1;
+        while (1) {
+          if (searched[temp_cnt] == ']' & searched[temp_cnt - 1] != '\\') {
+            break;
+          } else if (searched[temp_cnt + 1] == '{' & searched[temp_cnt] != '\\') {
+             temp_cnt += 1;
+             if (searched[temp_cnt + 1] != '?') {
+               temp_cnt += 1;
+               while (searched[temp_cnt] != '}') {
+                 temp_cnt += 1;
+               };
+               conditions_idxv2.push_back(temp_cnt);
+             } else {
+               temp_cnt += 1;
+               cur_par = 1;
+               while (1) {
+                 if (searched[temp_cnt] == '}' & searched[temp_cnt - 1] != '\\') {
+                   cur_par -= 1;
+                   if (cur_par == 0) {
+                     break;
+                   };
+                 } else if (searched[temp_cnt] == '{' & searched[temp_cnt - 1] != '\\') {
+                   cur_par += 1;
+                 };
+                 temp_cnt += 1;
+               };
+             };
+          } else {
+            if (searched[temp_cnt + 1] == '-') {
+              temp_cnt += 2;
+            };
+            conditions_idxv2.push_back(temp_cnt);
+          };
+          temp_cnt += 1;
+        };
+        conditions_idx = temp_cnt;
+        temp_cnt += 1;
+        if (temp_cnt < n) {
+          if (searched[temp_cnt] == '{') {
+            temp_cnt += 1;
+            if (searched[temp_cnt] == '?') {
+              cur_break_ptrn = "";
+              temp_cnt += 1;
+              cur_par = 1;
+              while (1) {
+                if (searched[temp_cnt] == '}' & searched[temp_cnt - 1] != '\\') {
+                  cur_par -= 1;
+                  if (cur_par == 0) {
+                    break;
+                  };
+                } else if (searched[temp_cnt] == '{' & searched[temp_cnt - 1] != '\\') {
+                  cur_par += 1;
+                };
+                cur_break_ptrn.push_back(searched[temp_cnt]);
+                temp_cnt += 1;
+              };
+              cur_mp = regex_findrlow(cur_break_ptrn, cur_x);
+              cur_it = cur_mp.begin();
+              rslt_mp = cur_it->second.begin();
+              if (rslt_mp->first) {
+                idx_v = cur_it->first;
+                n2 = idx_v[0];
+              };
+              temp_cnt += 1;
+            } else {
+              is_mult = 1;
+              ref_mult = int(searched[temp_cnt]) - 48;
+              while (searched[temp_cnt] != '}') {
+                ref_mult *= 10;
+                ref_mult += (int(searched[temp_cnt]) - 48);
+                temp_cnt += 1;
+              };
+              temp_cnt += 1;
+            };
+          };
+        };
+        cnt += 1;
+        jump_cnt = temp_cnt;
+      };
     };
     if (cnt < n) {
       cur_searched.push_back(searched[cnt]);
